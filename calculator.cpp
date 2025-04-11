0dbcb425fe78052cda76e849cb0705c00a331938
MPreethika <manepallypreethika@gmail.com>
2025-04-11 11:54:04 +0530

    created a calculator interface

---
+++ calculator.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+pair<double, double> get_input() {
+    double a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    return {a, b};
+}
+
+// Function placeholders for team members to implement (8 functions only)
+void add(double a, double b) {
+    // To be implemented by a team member
+}
+
+void subtract(double a, double b) {
+    // To be implemented by a team member
+}
+
+void multiply(double a, double b) {
+    // To be implemented by a team member
+}
+
+void divide(double a, double b) {
+    // To be implemented by a team member
+}
+
+void modulus(double a, double b) {
+    // To be implemented by a team member
+}
+
+void factorial(double a) {
+    // To be implemented by a team member
+}
+
+void logarithm(double a, double b) {
+    // To be implemented by a team member
+}
+
+void absolute(double a) {
+    // To be implemented by a team member
+}
+
+void calculator() {
+    cout << "\n Calculator Program" << endl;
+    auto [a, b] = get_input();
+    cout << "Values received: a = " << a << ", b = " << b << endl;
+
+    add(a, b);
+    subtract(a, b);
+    multiply(a, b);
+    divide(a, b);
+    modulus(a, b);
+    factorial(a);
+    logarithm(a, b);
+    absolute(a);
+}
+
+int main() {
+    calculator();
+    return 0;
+}
